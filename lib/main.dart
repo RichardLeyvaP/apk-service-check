87c0a02286beeaf0e2aa2e3a611d8f803b9bae96
RichardLeyvaP <richardleyvap1991@gmail.com>
2024-01-08 09:03:02 -0300

    creando proyecto

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      supportedLocales: AppLocalizations.supportedLocales,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      locale: Locale('es'),
+      title: 'Mi Aplicación',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context).title),
+      ),
+      body: Center(
+        child: Text(AppLocalizations.of(context).helloWorld),
+      ),
+    );
+  }
+}
