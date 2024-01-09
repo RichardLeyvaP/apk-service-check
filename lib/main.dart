1b7524916b24852233903bf1d4d9d0f42ab0a1d7
RichardLeyvaP <richardleyvap1991@gmail.com>
2024-01-09 15:55:15 -0300

    home listo

--- lib/main.dart
+++ lib/main.dart
@@ -11,108 +11,161 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 36, 35, 35),
         body: Column(
           children: [
             Expanded(
-              flex: 1,
-              child: Container(
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topRight,
-                    end: Alignment.bottomLeft,
-                    colors: [Colors.lightBlue, Colors.purple],
+              child: Stack(
+                children: [
+                  Positioned(
+                    top: 360,
+                    left: -70,
+                    child: Container(
+                      width: 180,
+                      height: 180,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        gradient: LinearGradient(
+                          begin: Alignment.topRight,
+                          end: Alignment.bottomLeft,
+                          colors: [Colors.purple, Colors.lightBlue],
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-                child: Stack(
-                  children: [
-                    Positioned(
-                      top: -30,
-                      right: -35,
-                      child: Container(
-                        width: 180,
-                        height: 180,
-                        decoration: const BoxDecoration(
-                          color: Color.fromARGB(96, 0, 0, 0),
-                          shape: BoxShape.circle,
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      bottom: -20,
-                      left: -30,
-                      child: Container(
-                        width: 150,
-                        height: 150,
-                        decoration: const BoxDecoration(
-                          color: Color.fromARGB(96, 0, 0, 0),
-                          shape: BoxShape.circle,
-                        ),
-                      ),
-                    ),
-                    Center(
-                      child: Padding(
-                        padding: const EdgeInsets.only(top: 60),
-                        child: Column(
+                  Column(
+                    children: [
+                      Expanded(
+                        flex: 5,
+                        child: Stack(
                           children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(50),
-                              child: Image.network(
-                                'https://th.bing.com/th/id/OIP.dkGMi3fxsO7WBxzYXyFWKgAAAA?rs=1&pid=ImgDetMain',
-                                width: 100,
-                                height: 100,
-                                fit: BoxFit.cover,
+                            Positioned(
+                              top: -70,
+                              right: -75,
+                              child: Container(
+                                width: 220,
+                                height: 220,
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  gradient: LinearGradient(
+                                    begin: Alignment.topRight,
+                                    end: Alignment.bottomLeft,
+                                    colors: [
+                                      Color.fromARGB(255, 11, 70, 231),
+                                      Color.fromARGB(255, 138, 199, 240),
+                                    ],
+                                  ),
+                                ),
                               ),
                             ),
-                            const Text(
-                              'Mariela Laos Zamora',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
+                            Center(
+                              child: Padding(
+                                padding: const EdgeInsets.only(top: 100),
+                                child: Column(
+                                  children: [
+                                    CircleAvatar(
+                                      radius: 70,
+                                      backgroundColor:
+                                          Color.fromARGB(159, 127, 171, 238),
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(80),
+                                        child: Image.network(
+                                          'https://th.bing.com/th/id/OIP.dkGMi3fxsO7WBxzYXyFWKgAAAA?rs=1&pid=ImgDetMain',
+                                          width: 130,
+                                          height: 130,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    ),
+                                    const Text(
+                                      'Mariela Laos Zamora',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w700,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                    const Text(
+                                      'Ing. Informática',
+                                      style: TextStyle(
+                                        color:
+                                            Color.fromARGB(164, 255, 255, 255),
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        // Acción para cerrar la cuenta
+                                      },
+                                      style: ButtonStyle(
+                                        backgroundColor:
+                                            MaterialStateProperty.all<Color>(
+                                                const Color.fromARGB(
+                                                    255, 87, 86, 86)),
+                                        shape: MaterialStateProperty.all<
+                                            OutlinedBorder>(
+                                          RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(
+                                                30.0), // Ajusta el valor según tus necesidades
+                                          ),
+                                        ),
+                                      ),
+                                      child: const Text('Cerrar mi cuenta'),
+                                    ),
+                                  ],
+                                ),
                               ),
-                            ),
-                            ElevatedButton(
-                              onPressed: () {
-                                // Acción para cerrar la cuenta
-                              },
-                              child: const Text('Cerrar mi cuenta'),
                             ),
                           ],
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Expanded(
-                flex: 1,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
+                      Stack(
+                        children: [
+                          Container(
+                            height: 50, // Altura del Stack
+                            color: Colors.transparent, // Color transparente
+                          ),
+                        ],
+                      ),
+                      Expanded(
+                          flex: 4,
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(30),
+                                  topRight: Radius.circular(30)),
+                              color: Color.fromARGB(255, 87, 86,
+                                  86), // Ajusta el valor de opacidad según tus necesidades
+                            ),
+                          )),
+                    ],
                   ),
-                  child: Container(
-                    color: const Color.fromARGB(214, 0, 0, 0),
+                  Positioned(
+                    top: 420,
+                    left: 0,
+                    right: 0,
                     child: Column(
                       children: [
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
-                            buildCard(Icons.home),
-                            buildCard(Icons.search),
+                            buildCard(Icons.home, 'Home'),
+                            buildCard(Icons.search, 'Buscar'),
                           ],
                         ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
-                            buildCard(Icons.favorite),
-                            buildCard(Icons.settings),
+                            buildCard(Icons.favorite, 'Favoritos'),
+                            buildCard(Icons.settings, 'Opciones'),
                           ],
                         ),
                       ],
                     ),
                   ),
-                )),
+                ],
+              ),
+            ),
           ],
         ),
         bottomNavigationBar: BottomNavigationBar(
@@ -143,18 +196,40 @@ class MyApp extends StatelessWidget {
     );
   }
 
-  Widget buildCard(IconData icon) {
+  Widget buildCard(IconData icon, String labelText) {
     return Card(
-      elevation: 5,
+      color: Colors.transparent,
+      elevation: 0,
       child: Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           borderRadius: BorderRadius.all(Radius.circular(20)),
-          color: Colors.white.withOpacity(
-              0.8), // Ajusta el valor de opacidad según tus necesidades
+          color: Color.fromARGB(255, 48, 47,
+              47), // Ajusta el valor de opacidad según tus necesidades
         ),
         width: 170,
         height: 170,
-        child: Icon(icon),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              backgroundColor: const Color.fromARGB(255, 87, 86, 86),
+              radius: 30,
+              child: Icon(
+                icon,
+                color: Colors.lightBlue,
+                size: 35,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              labelText,
+              style: const TextStyle(
+                  color: Colors.white, fontWeight: FontWeight.w700),
+            ),
+          ],
+        ),
       ),
     );
   }
