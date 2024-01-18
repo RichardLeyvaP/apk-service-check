83430403f950f31e650927d32969200e6968f4f2
RichardLeyvaP <richardleyvap1991@gmail.com>
2024-01-18 00:20:10 -0300

    agregando formularios para mandar datos a la api para generar el pdf

---
+++ lib/Controllers/login.controller.dart
@@ -0,0 +1,175 @@
+// ignore_for_file: depend_on_referenced_packages
+/*
+import 'package:get/get.dart';
+import 'package:turnopro_apk/Routes/index.dart';
+import 'package:turnopro_apk/get_connect/repository/user.repository.dart';
+
+class LoginController extends GetxController {
+  @override
+  void onReady() {
+    super.onReady();
+    Future.delayed(const Duration(seconds: 2), () {
+      isLoading = false;
+      update();
+    });
+  }
+
+  bool maintainClockStatus = false;
+  UserRepository usuarioLg = UserRepository();
+  //*************************/
+  String nameUserLoggedIn = '';
+  String userLoggedIn = '';
+  String tokenUserLoggedIn = '';
+  int idUserLoggedIn = -2023991991;
+  String emailUserLoggedIn = '';
+  String chargeUserLoggedIn = '';
+  int? idProfessionalLoggedIn;
+  int? branchIdLoggedIn;
+  //*************************/
+  bool isLoading = true;
+  String pagina = 'nothing';
+  bool obscureText = true;
+  String qrRead = '';
+  bool incorrectFields = false;
+  String greeting = 'Buenos días ';
+
+  void setMaintainClockStatus() {
+    maintainClockStatus = true;
+    update();
+  }
+
+  void getGreeting() {
+    // Obtener la hora actual
+    DateTime now = DateTime.now();
+
+    // Obtener la hora del día
+    int hour = now.hour;
+
+    // Determinar el saludo según la hora
+
+    if (hour < 12) {
+      greeting = 'Buenos días ';
+    } else if (hour < 18) {
+      greeting = 'Buenas tardes ';
+    } else {
+      greeting = 'Buenas noches ';
+    }
+    update();
+  }
+
+  void qrReading(String? qr) {
+    print('entre aqui a el controlador de lectura del QR${qr.toString()}');
+    // Map<String, dynamic> jsonMap = json.decode(qr.toString());
+    // print('......................Objeto JSON: $jsonMap');
+    // String nombre = jsonMap['nombre']; // Accede al valor del campo "nombre"
+    // int edad = jsonMap['edad']; // Accede al valor del campo "edad"
+    // print(nombre);
+    // print(edad);
+  }
+
+  Future<void> loginGetIn(String u, String p) async {
+    final ClientsScheduledController clientsScheduledController =
+        Get.find<ClientsScheduledController>();
+    String email = u.toString(), pass = p.toString();
+    incorrectFields = false;
+    try {
+      Map<String, dynamic>? result; //INICIALIZANDO A NULL
+      result = await usuarioLg.getUserLoggedIn(email, pass);
+
+      if (result != null) {
+        //*******Asignando Valores*****/
+        nameUserLoggedIn = result['name'];
+        userLoggedIn = result['userName'];
+        tokenUserLoggedIn = result['token'];
+        idUserLoggedIn = result['id'];
+        emailUserLoggedIn = result['email'];
+        chargeUserLoggedIn = result['charge'];
+        idProfessionalLoggedIn = result['professional_id'];
+        branchIdLoggedIn = result['branch_id'];
+
+        //*******Asignando Valores*****/
+        print('branchIdLoggedIn***************************: $branchIdLoggedIn');
+        print('TOKEN***************************: $tokenUserLoggedIn');
+        print('ID-Profess***************************: $idProfessionalLoggedIn');
+
+        if (tokenUserLoggedIn != '' &&
+            nameUserLoggedIn != '' &&
+            emailUserLoggedIn != '') {
+          //Define el tipo de saludo
+          getGreeting();
+
+          if (chargeUserLoggedIn == "Barbero") {
+            //aqui cargo la cola del barbero para poder tener en el home al siguiente de la cola inicialmente
+            print('estoy aqui al cargar datos del controlador de client');
+            clientsScheduledController.setCloseIesperado(true);
+            await clientsScheduledController.fetchClientsScheduled(
+                idProfessionalLoggedIn, branchIdLoggedIn);
+            print(' ya no llegue aqui voy a cargar la pagina del profesional');
+
+            print('***************SOY BARBERO*************');
+            pagina = '/Professional';
+            update();
+            Get.offAllNamed('/Professional');
+          } else if (chargeUserLoggedIn == "Encargado") {
+            print('***************SOY ENCARGADO*************');
+            pagina = '/HomeResponsible';
+            update();
+            Get.offAllNamed('/HomeResponsible');
+          } else if (chargeUserLoggedIn == "Tecnico") {
+            print('***************SOY TECNICO CAPILAR*************');
+            pagina = '/HomeTecnico';
+            update();
+            Get.offAllNamed('/HomeTecnico');
+          }
+        }
+
+        update();
+      } //cierre if (result != null) {
+      else {
+        incorrectFields = true;
+        update();
+        print(' result == null por eso no entro');
+      }
+    } catch (e) {
+      print('errorrrrrr:$e');
+    }
+  }
+
+  Future<void> exit(String token) async {
+    try {
+      if (token != '') {
+        Map<String, dynamic>? result; //INICIALIZANDO A NULL
+        result = await usuarioLg.userLogout(token);
+        if (result != null) {
+          await clearSessionData();
+          Get.offAllNamed('/loginNewPage');
+        } else {
+          print('NO CERRO SECION');
+        }
+      } else
+        print('ERROR: -----> Revisar que el token esta llegando aqui vacio');
+    } catch (e) {
+      print('Erroor:$e');
+    }
+  }
+
+  Future<void> clearSessionData() async {
+    nameUserLoggedIn = '';
+    userLoggedIn = '';
+    tokenUserLoggedIn = '';
+    idUserLoggedIn = -2023991991;
+    chargeUserLoggedIn = '';
+    emailUserLoggedIn = '';
+    idProfessionalLoggedIn = null;
+    branchIdLoggedIn = null;
+    pagina = 'nothing';
+    incorrectFields = false;
+    update();
+  }
+
+  void togglePasswordVisibility() {
+    obscureText = !obscureText;
+    update();
+  }
+}
+*/
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,12 +16,14 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -25,13 +31,13 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int currentStep = 0;
   List<TextEditingController> textControllers =
-      List.generate(6, (index) => TextEditingController());
+      List.generate(7, (index) => TextEditingController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Service Check PDF'),
+        title: const Text('Service Check PDF'),
       ),
       body: Stepper(
         currentStep: currentStep,
@@ -40,23 +46,28 @@ class _MyHomePageState extends State<MyHomePage> {
         steps: [
           Step(
             content: _buildFormStep1(),
-            title: const Text('Form 1'),
+            title: const Text('DADOS DO CLIENTE'),
             isActive: currentStep == 0,
           ),
           Step(
             content: _buildFormStep2(),
-            title: const Text('Form 2'),
+            title: const Text('DADOS DO INSTRUMENTO'),
             isActive: currentStep == 1,
           ),
           Step(
             content: _buildFormStep3(),
-            title: const Text('Form 3'),
+            title: const Text('AFERICÃO/CALIBRACÃO'),
             isActive: currentStep == 2,
           ),
           Step(
             content: _buildFormStep4(),
-            title: const Text('Form 4'),
+            title: const Text('PATRÕES UTILIZADOS'),
             isActive: currentStep == 3,
+          ),
+          Step(
+            content: _buildFormStep5(),
+            title: const Text('CONVERTER (Final)'),
+            isActive: currentStep == 4,
           ),
         ],
         type: StepperType.vertical,
@@ -68,21 +79,40 @@ class _MyHomePageState extends State<MyHomePage> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        _buildTextField('Field 1'),
-        _buildTextField('Field 2'),
-        _buildTextField('Field 3'),
-        _buildTextField('Field 4'),
-        _buildTextField('Field 5'),
-        _buildTextField('Field 6'),
+        _buildTextField('Empresa'), //branchName
+        _buildTextField('Cidade/Estado'), //cityState
+        _buildTextField('Contato'), //contact
       ],
     );
   }
 
   Widget _buildFormStep2() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
+    return Row(
       children: [
-        // Add your fields for Step 2 here
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _buildTextField('No Relatório'), //numberRelatorie
+              _buildTextField('Tag'), //tag
+              _buildTextField('Descricão'), //direction
+              _buildTextField('Fabricante'), //fabricante
+              _buildTextField('Funcão no Proceso'), //functionProceso
+            ],
+          ),
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _buildTextField('Faixa de Calibracão'), //faixa
+              _buildTextField('Variável Medida'), //medida
+              _buildTextField('F.R.E'), //fre
+              _buildTextField('Data Calibracão'), //dataCalibration
+              _buildTextField('Data Prox. Calibracão'), //dataNextCalibration
+            ],
+          ),
+        ),
       ],
     );
   }
@@ -91,23 +121,56 @@ class _MyHomePageState extends State<MyHomePage> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        // Add your fields for Step 3 here
+        _buildTextField('aplicada25'), //aplicada25
+        _buildTextField('aplicada50'), //aplicada50
+        _buildTextField('aplicada75'), //aplicada75
+        _buildTextField('aplicada100'), //aplicada100
       ],
     );
   }
 
   Widget _buildFormStep4() {
+    return Row(
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _buildTextField('Instrumento Patrão'), //instrument_padrao
+              _buildTextField('Certificado'), //certificado
+              _buildTextField('Servicos Executados'), //service_execute
+              _buildTextField('ART'), //art
+              _buildTextField('Técnico'), //tecnico
+            ],
+          ),
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _buildTextField('Modelo'), //model
+              _buildTextField('Data afericão'), //date_aferica
+              _buildTextField('Enegenheiro'), //ingenier
+              _buildTextField('Data'), //data
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildFormStep5() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         // Add your fields for Step 4 here
         ElevatedButton(
           onPressed: () => _generatePdf(),
-          child: Text('Generar PDF'),
+          child: Text('Gerar PDF'),
         ),
-        TextButton(
-          onPressed: () => _cancel(),
-          child: Text('Cancelar'),
+        ElevatedButton(
+          onPressed: () => _generatePdf(),
+          child: Text('Salvar no banco de dados'),
         ),
       ],
     );
@@ -117,7 +180,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: TextField(
-        controller: textControllers[currentStep],
+        //controller: textControllers[currentStep],
         decoration: InputDecoration(
           labelText: labelText,
         ),
@@ -126,7 +189,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _nextStep() {
-    if (currentStep < 3) {
+    if (currentStep < 4) {
       setState(() {
         currentStep++;
       });
